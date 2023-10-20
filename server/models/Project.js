cad19893e7d546a623dda689c2da83700f3e7d14
Jimmy Vela <jimmyvela06@.com>
2023-10-20 01:09:11 -0500

    setup server/graphQL/modals/database

---
+++ server/models/Project.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ProjectsSchema = new mongoose.Schema({
+  name: { type: String },
+  description: { type: String },
+  status: {
+    type: String,
+    enum: ['Not Started', 'In Progress', 'Completed'],
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+  },
+});
+
+module.exports = mongoose.model('Projects', ProjectsSchema);
