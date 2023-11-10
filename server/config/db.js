dd212766a1a3dc60fa061610108a3d13354ff1d6
Jimmy Vela <jimmyvela06@.com>
2023-11-10 01:45:13 -0600

    updated apollo server

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,32 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   const conn = await mongoose.connect(process.env.MONGO_URI);
+//   conn.mongoose.set('strictQuery', true);
+//   console.log(`CONNECTION: MongoDB Has Been Connected TO ${conn.connection.host}`);
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(
-    'mongodb+srv://jimmyvela6:abc123ab12@projectmanagment.dlmavbg.mongodb.net/mgmt_db?retryWrites=true&w=majority'
-  );
-  conn.mongoose.set('strictQuery', true);
-  introspection: true;
-  console.log(`CONNECTION: MongoDB Has Been Connected TO ${conn.connection.host}`.blue.bold);
-};
+mongoose.connect(
+  process.env.MONGO_URI || 'mongodb+srv://jimmyvela6:abc123ab12@projectmanagment.dlmavbg.mongodb.net/mgmt_db?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-module.exports = connectDB;
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+  process.exit(1);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
